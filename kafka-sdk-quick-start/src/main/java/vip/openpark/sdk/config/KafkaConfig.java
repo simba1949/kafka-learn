563910ead68204ec3b4076e9b65a795c7eb9c4c9
openpark <openpark.vip@outlook.com>
2024-04-15 20:46:54 +0800

    feature：ConsumerConfig、ProducerConfig 常量配置类

--- kafka-sdk-quick-start/src/main/java/vip/openpark/sdk/config/KafkaConfig.java
+++ kafka-sdk-quick-start/src/main/java/vip/openpark/sdk/config/KafkaConfig.java
@@ -4,6 +4,8 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -26,13 +28,13 @@ public class KafkaConfig {
         // 设置客户端id（可以使用 ProducerConfig.CLIENT_ID_CONFIG 里面常量）
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, InetAddress.getLocalHost().getHostName());
         // kafka 集群配置，多个以半角逗号分隔
-        properties.put("bootstrap.servers", "172.17.35.120:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.17.35.120:9092");
         // 0：不保证消息的可靠性，1：保证消息的可靠性，-1：确保消息至少被一次的提交，all：确保消息被全部提交
-        properties.put("acks", "all");
-        properties.put("retries", 0);
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.RETRIES_CONFIG, 0);
         // 序列化
-        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         return new KafkaProducer<>(properties);
     }
@@ -48,12 +50,12 @@ public class KafkaConfig {
         // 设置客户端id（可以使用 ConsumerConfig.CLIENT_ID_CONFIG 里面常量）
         properties.put(ConsumerConfig.CLIENT_ID_CONFIG, InetAddress.getLocalHost().getHostName());
         // 设置消费组
-        properties.put("group.id", "hi-kafka-group");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "hi-kafka-group");
         // kafka 集群配置，多个以半角逗号分隔
-        properties.put("bootstrap.servers", "172.17.35.120:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.17.35.120:9092");
         // 反序列化器
-        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
         return new KafkaConsumer<>(properties);
     }
